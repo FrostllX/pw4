e94c4f49a204ad6c807f639eea1307057cabde97
FrostllX <frost3333@yandex.ru>
2025-10-20 21:24:28 +0300

    add env v3

--- tests/env.spec.js
+++ tests/env.spec.js
@@ -4,7 +4,7 @@ test('test', async ({ page }) => {
   // arrange или настройка, предусловие
   await page.goto('https://todomvc.com/examples/vue/dist/#/');
   console.log(process.env);
-  console.log(process.env.secret);
-  console.log(process.env.step);
+  console.log(process.env.SECRET);
+  console.log(process.env.STEP);
   await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toBeEmpty();
 });
